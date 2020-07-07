a51f5875fc7b8c596ef6a141961be40c1de5a80d
Kent Richards <kent-richards@outlook.com>
2020-07-07 16:21:10 -0300

    the big fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// Change CSS styles when element is stuck:
-// https://stackoverflow.com/a/56678169
-// In example change 'top' to 'bottom' in `nav`
-// And make `header` much taller
-
 const listIcon = document.getElementById("header-list");
 const sidebar = document.getElementsByTagName("aside")[0];
 
@@ -22,3 +17,14 @@ closeIcon.addEventListener("click", () => {
   sidebar.classList.add("hide");
   sidebar.classList.remove("block");
 });
+
+// Change CSS styles when element is stuck:
+// https://stackoverflow.com/a/56678169
+// In example change 'top' to 'bottom' in `nav`
+// And make `header` much taller
+const observer = new IntersectionObserver(
+  ([e]) => e.target.toggleAttribute("stuck", e.intersectionRatio < 1),
+  { threshold: [1] }
+);
+
+observer.observe(document.getElementsByClassName("sticky")[0]);
